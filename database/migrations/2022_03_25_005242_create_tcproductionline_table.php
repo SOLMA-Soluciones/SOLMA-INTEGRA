2a5e720d60ec9dfc26ff0c719bb9ce27497e5cb1
AlejandroHDZ2021 <alexhernandez.pro98@gmail.com>
2022-03-29 12:52:57 -0600

    table tcproductionline modified

--- database/migrations/2022_03_25_005242_create_tcproductionline_table.php
+++ database/migrations/2022_03_25_005242_create_tcproductionline_table.php
@@ -14,7 +14,7 @@ class CreateTcproductionlineTable extends Migration
     public function up()
     {
         Schema::create('tcproductionline', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->timestamps();
         });
