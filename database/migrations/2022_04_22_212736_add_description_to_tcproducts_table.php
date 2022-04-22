78f967aeea0fa7ff70c12e56d6e52fae22e5144d
AlejandroHDZ2021 <alexhernandez.pro98@gmail.com>
2022-04-22 17:05:51 -0500

    added description field

--- database/migrations/2022_04_22_212736_add_description_to_tcproducts_table.php
+++ database/migrations/2022_04_22_212736_add_description_to_tcproducts_table.php
@@ -14,7 +14,7 @@ class AddDescriptionToTcproductsTable extends Migration
     public function up()
     {
         Schema::table('tcproducts', function (Blueprint $table) {
-            $table->string('description')->nullable()->after('part_number');
+            //
         });
     }
 
@@ -26,7 +26,7 @@ class AddDescriptionToTcproductsTable extends Migration
     public function down()
     {
         Schema::table('tcproducts', function (Blueprint $table) {
-            $table->dropColumn('description');
+            //
         });
     }
 }
